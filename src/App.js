72983ad9a53ee87b4a05d481908cfb1df5c10ba3
Chyngyz Bekbolotov$ <you@example.com>
2021-02-24 15:05:53 +0600

    added fail app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import React from 'react';
 import HelloWorld from "./components/HelloWorld";
-
+import ByeWorlds from "./components/ByeWorlds";
 function App() {
   return (
     <div className="App">
      <HelloWorld />
+     <HelloWorld />
+     <HelloWorld />
+     <HelloWorld />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+    <ByeWorlds />
+    <ByeWorlds />
+    <ByeWorlds />
+    <ByeWorlds />
+    </div>
+  );
+}
+
+
+
+function App() {
+  return (
+    <div className="App">
+  <Hochu v more />
+  <Hochu v more />
+  <Hochu v more />
+  <Hochu v more />
     </div>
   );
 }
